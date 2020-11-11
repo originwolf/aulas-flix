749f029a3282520d635fa19c680a4fd77ccf3e08
Pedro Martins <pedrogmhead@gmail.com>
2020-11-11 09:29:48 -0300

    added necessary scripts

--- src/App.js
+++ src/App.js
@@ -11,20 +11,12 @@ function App() {
         url = {dadosIniciais.categorias[0].videos[0].url}
         videoDescription = {"By Pedro Martins"}
       />
+
       <div className="py-5 bg-light-gray">
-        <Carousel
-          ignoreFirstVideo
-          category={dadosIniciais.categorias[0]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[1]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[2]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[3]}
-        />
+        <Carousel ignoreFirstVideo category={dadosIniciais.categorias[0]} />
+        <Carousel category={dadosIniciais.categorias[1]} />
+        <Carousel category={dadosIniciais.categorias[2]} />
+        <Carousel category={dadosIniciais.categorias[3]} />
       </div>
     </article>
   );
